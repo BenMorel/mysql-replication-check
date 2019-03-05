6792d88d010b46b60cdaf6e1ec112fc67a3817f4
Benjamin Morel <benjamin.morel@gmail.com>
2019-03-05 15:38:57 +0100

    Fix bug checking every table several times
    When several patterns were provided in --tables or --ignore-tables, and a table matched several patterns, the table was added several times to the list of tables to check.

--- mysql-replication-check.php
+++ mysql-replication-check.php
@@ -121,6 +121,7 @@ function filterTables(array $tables, $pattern, $inverse) {
 
             if ($match !== $inverse) {
                 $result[] = $table;
+                break;
             }
         }
     }
